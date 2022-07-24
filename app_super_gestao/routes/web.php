64aa9a6edcdcf793d7a5f483b6a1ceeea69500a6
LeonardoAlmeidaRibeiro <leonardo.a.ribeiro.21@gmail.com>
2022-07-24 18:40:03 -0300

    Rotas Avançadas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -16,13 +16,26 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return "Olá, seja bem vindo a esse curso!";
 // });
-Route::get('/', 'PrincipalController@principal');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-Route::get('/contato', 'ContatoController@contato');
-// Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}', function (string $nome = 'Desconhecido', string $categoria = 'Informação', string $assunto = 'contato', string $mensagem = 'Mensagem nao informada') {
-//     echo "Estamos aqui: " . $nome . " - " . $categoria . " -    " . $assunto . " - " . $mensagem;
-// });
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobre-nos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
-Route::get('/contato/{nome}/{categoria_id}', function (string $nome = 'Desconhecido', int $categoria_id = 1)  {
-    echo "Estamos aqui: " . $nome .'<br>'. " Categoria: " . $categoria_id;
-})->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
+Route::get('/login', function(){ return 'loign';});
+
+//app
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos';})->name('app.produtos');
+});
+Route::get('/rota1', function () {
+    echo 'rota1';
+})->name('site.rota1');
+
+Route::get('/rota2', function () {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+//Route::redirect('rota2','rota1');
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'. route('site.index').'">clique aqui</a>';
+});
