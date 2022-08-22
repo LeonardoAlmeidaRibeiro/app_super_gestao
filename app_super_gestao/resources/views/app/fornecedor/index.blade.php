bdfef132565f21dd509d0966c444594828083ce0
LeonardoAlmeidaRibeiro <leonardo.a.ribeiro.21@gmail.com>
2022-08-21 21:33:20 -0300

    Atualizacao do projeto

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,57 +1,45 @@
-<h2>teste fornecedor</h2>
-{{-- Comentário no Blade Teste --}}
+<h3>Fornecedor</h3>
+
 @php
-//teste
-/*
-     esse é padrão
-     */
+    /*
+    if(empty($variavel)) {} //retornar true se a variável estiver vazia
+    - ''
+    - 0
+    - 0.0
+    - '0'
+    - null
+    - false
+    - array()
+    - $var
+    */
 @endphp
 
-
-
-@if (count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3> existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3> existem vários fornecedores cadastrados</h3>
-@else
-    <h3> ainda não existem fornecedores cadastrados</h3>
-@endif
-
-<hr>
-
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-<br>
-Status: {{ $fornecedores[0]['status'] }}
-<br>
-@if( !($fornecedores[0]['status'] == 'S') )
-    Fornecedor inativo
-@endif
-<br>
-@unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condição for false -->
-    Fornecedor inativo
-@endunless
-<br>
-
-<hr>
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[0]['status'] }}
-    <br>
-    CNPJ: {{ $fornecedores[0]['cnpj'] ?? '' }}
-    <br>
-    Telefone: ({{ $fornecedores[0]['ddd'] ?? '' }}) {{ $fornecedores[0]['telefone'] ?? '' }}
-    @switch($fornecedores[0]['ddd'])
-        @case ('11')
-            São Paulo - SP
-            @break
-        @case ('32')
-            Juiz de Fora - MG
-            @break
-        @case ('85')
-            Fortaleza - CE
-            @break
-        @default
-            Estado não indentificado
-    @endswitch
+
+    @forelse($fornecedores as $indice => $fornecedor)
+        Iteração atual: {{ $loop->iteration }}
+        <br>
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br>
+        Status: {{ $fornecedor['status'] }}
+        <br>
+        CNPJ: {{ $fornecedor['cnpj'] ?? '' }}
+        <br>
+        Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        <br>
+        @if($loop->first)
+            Primeira iteração no loop
+
+            <br>
+            Total de registros: {{ $loop->count }}
+        @endif
+
+        @if($loop->last)
+            Última iteração no loop
+        @endif
+        <hr>
+    @empty
+        Não existem fornecedores cadastrados!!!
+    @endforelse
 @endisset
+
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -12,33 +12,25 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-// Route::get('/', function () {
-//     return "Olá, seja bem vindo a esse curso!";
-// });
-Route::get('/', 'PrincipalController@principal')->name('site.index');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobre-nos');
-Route::get('/contato', 'ContatoController@contato')->name('site.contato');
-
-Route::get('/login', function(){ return 'loign';});
-
-//app
-Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){ return 'Clientes';})->name('app.clientes');
-    Route::get('/fornecedores','FornecedorController@index')->name('app.fornecedor');
-    Route::get('/produtos', function(){ return 'Produtos';})->name('app.produtos');
+/*
+Route::get('/', function () {
+    return 'Olá, seja bem vindo ao curso!';
 });
-Route::get('/rota1', function () {
-    echo 'rota1';
-})->name('site.rota1');
+*/
 
-Route::get('/rota2', function () {
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
-Route::get('teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
 
-//Route::redirect('rota2','rota1');
-Route::fallback(function(){
-    echo 'A rota acessada não existe. <a href="'. route('site.index').'">clique aqui</a>';
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
 });
