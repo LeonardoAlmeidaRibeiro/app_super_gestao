ec124d7c0c2f057d20f46f5a20e4cdbae8c0e2d4
LeonardoAlmeidaRibeiro <leonardo.a.ribeiro.21@gmail.com>
2022-08-29 14:11:40 -0300

    gravando no banco de dados

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,21 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\SiteContato;
 class ContatoController extends Controller
 {
     public function contato(Request $request){
         
 
-        echo '<prev>';
-        print_r($request->all());
-        echo '</prev>';
-        echo $request->input('nome');
-        echo '<br>';
-        echo  $request->input('email');
+    //    $contato = new SiteContato();
+    //    $contato->nome = $request->input('nome');
+    //    $contato->telefone = $request->input('telefone');
+    //    $contato->email = $request->input('email');
+    //    $contato->motivo_contato = $request->input('motivo_contato');
+    //    $contato->mensagem = $request->input('mensagem');
 
-        //dd($request);
-        //var_dump($_GET);
+    //    //print_r($contato->getAttributes());
+    //    $contato->save();
+
+    $contato = new SiteContato();
+    $contato->fill($request->all());
+    $contato->save();
         return view('site.contato',['titulo'=>'Contato']);
     }
 }
--- app_super_gestao/app/SiteContato.php
+++ app_super_gestao/app/SiteContato.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteContato extends Model
 {
-    //
+    
+    protected $fillable = [
+        'nome',
+        'telefone',
+        'email',
+        'motivo_contato',
+        'mensagem'
+
+    ];
 }
