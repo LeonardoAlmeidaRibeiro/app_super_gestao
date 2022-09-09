40f6d778be1022f6be71992628b8b1fbd2d94230
LeonardoAlmeidaRibeiro <leonardo.a.ribeiro.21@gmail.com>
2022-09-08 22:48:01 -0300

    Middleware

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -18,21 +18,20 @@ Route::get('/', function () {
 });
 */
 
-Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/', 'PrincipalController@principal')->name('site.index')->middleware('log.acesso');
+
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
-Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
-
-Route::prefix('/app')->group(function() {
+Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function() {
     Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
     Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
     Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
-
 Route::get('pdf', 'PdfController@geraPdf');
 
 Route::fallback(function() {
