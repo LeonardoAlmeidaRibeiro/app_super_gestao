0dbb632c9d5e5f0fd7c637538bddd7aa5b382992
LeonardoAlmeidaRibeiro <leonardo.a.ribeiro.21@gmail.com>
2023-01-07 21:18:14 -0300

    Criando relacionamentos de  um para muitos

--- app_super_gestao/resources/views/app/produto/index.blade.php
+++ app_super_gestao/resources/views/app/produto/index.blade.php
@@ -19,17 +19,19 @@
 
         <div class="informacao-pagina">
             <div style="width: 90%; margin-left: auto; margin-right: auto;">
-         <table border="1" width="100%">
+                <table border="1" width="100%">
                     <thead>
                         <tr>
                             <th>Nome</th>
                             <th>Descrição</th>
+                            <th>Nome do Fornecedor</th>
+                            <th>Site do Fornecedor</th>
                             <th>Peso</th>
                             <th>Comprimento</th>
                             <th>Altura</th>
                             <th>Largura</th>
                             <th coldspan=4>Unidade ID</th>
-                            
+
                         </tr>
                     </thead>
 
@@ -38,28 +40,33 @@
                             <tr>
                                 <td>{{ $produto->nome }}</td>
                                 <td>{{ $produto->descricao }}</td>
+                                <td>{{ $produto->fornecedor->nome }}</td>
+                                 <td>{{ $produto->fornecedor->site }}</td>
                                 <td>{{ $produto->peso }}</td>
-                                <td>{{ $produto->itemDetalhe->comprimento ?? ''}}</td>
-                                <td>{{ $produto->itemDetalhe->altura ?? ''}}</td>
-                                <td>{{ $produto->itemDetalhe->largura ?? ''}}</td>
+                                <td>{{ $produto->itemDetalhe->comprimento ?? '' }}</td>
+                                <td>{{ $produto->itemDetalhe->altura ?? '' }}</td>
+                                <td>{{ $produto->itemDetalhe->largura ?? '' }}</td>
                                 <td>{{ $produto->unidade_id }}</td>
-                                <td><a href="{{route('app.produto.show',['produto'=>$produto->id])}}">Visualizar</a></td>
+                                <td><a href="{{ route('app.produto.show', ['produto' => $produto->id]) }}">Visualizar</a></td>
                                 <td>
-                                <form id="form_{{$produto->id}}" method="post" action="{{route('app.produto.destroy',['produto'=>$produto->id])}}">
-                                @method('DELETE')
-                                @csrf
-                               
-                                    <a href="#" onclick="document.getElementById('form_{{$produto->id}}').submit()">Excluir</a>
-                                </form>
+                                    <form id="form_{{ $produto->id }}" method="post"
+                                        action="{{ route('app.produto.destroy', ['produto' => $produto->id]) }}">
+                                        @method('DELETE')
+                                        @csrf
+
+                                        <a href="#"
+                                            onclick="document.getElementById('form_{{ $produto->id }}').submit()">Excluir</a>
+                                    </form>
                                 </td>
-                                <td><a href="{{route('app.produto.edit',['produto'=>$produto->id])}}">Editar</a></td>
+                                <td><a href="{{ route('app.produto.edit', ['produto' => $produto->id]) }}">Editar</a></td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
 
                 {{ $produtos->appends($request)->links() }}
-                <p>Exibindo {{ $produtos->count() }}  de produtoes de {{ $produtos->total() }} (de {{ $produtos->firstItem() }} a {{ $produtos->lastItem() }})
+                <p>Exibindo {{ $produtos->count() }} de produtoes de {{ $produtos->total() }} (de
+                    {{ $produtos->firstItem() }} a {{ $produtos->lastItem() }})
 
 
             </div>
